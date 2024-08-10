89ed07f0c8e8822aa867f7746b0f7c5bdc9b4649
Savenok <romasav2017@gmail.com>
2024-08-11 00:49:54 +0300

    20.07.2023

---
+++ ControllWork/code/Counter.java
@@ -0,0 +1,23 @@
+﻿package code;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+class Counter {
+    public  Scanner scanner = new Scanner(System.in);
+    int count = 0;
+    public  int add() throws IOException{
+        count = count + 1;
+        System.out.println("Enter name Animal: ");
+        String name = scanner.nextLine();
+        if (name.isEmpty() != false) throw new IOException("name is empty");
+        System.out.println("Enter age Animal: ");
+        String age = scanner.nextLine();
+        if (age.isEmpty() != false) throw new IOException(" age is empty");
+        System.out.println("Enter one of the command Animal: ");
+        String command = scanner.nextLine();
+        if (command.isEmpty() != false) throw new IOException(" commnds is empty");
+        System.out.println("Animal was added. Name:" + name + ", age:" + age + ", command" + command);
+        return count;
+    }
+}
---
+++ ControllWork/code/work.java
@@ -0,0 +1,53 @@
+﻿package code;
+import code.Counter;
+import java.util.Scanner; 
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class work {
+    public static ArrayList<String> commands = new ArrayList<String>();
+    public static Scanner scanner = new Scanner(System.in);
+    public  int count = 0;
+    public static void main(String args[]) {
+        menu();
+    }
+    
+    public static void AddNewCommands(){
+        System.out.println("Enter command: ");
+        String command = scanner.nextLine();
+        commands.add(command);
+        System.out.println("Commands was add!");
+    }
+
+    public static void menu(){
+        System.out.println("What do what to do?: \n1 - Add new animal\n2 - Add new Command\n3 - View Commands");    
+        int value = scanner.nextInt();
+        if (value == 1){
+            try{
+                Counter ct = new Counter();
+                ct.add();
+                menu();    
+            } catch (IOException e) {
+                menu();
+            }
+        } else if(value == 2){
+            AddNewCommands();
+        } else if (value == 3){
+            PrintCOmmands();
+        } else {
+            System.exit(0);
+        }
+        
+    }
+    public static void PrintCOmmands(){
+        if (commands.size() == 0){
+            System.out.println("List is empty");
+            menu();
+        } else {
+            for(String command: commands){
+                System.out.println(command);
+            }
+        }    
+    }
+}
+
